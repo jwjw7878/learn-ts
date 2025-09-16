59306c89394d0073aad159ad1076bf534c7c204f
pjw <jwp000708@google.com>
2025-09-16 17:34:37 +0900

    반대 메시지

---
+++ vote-app/vote.ts
@@ -0,0 +1,14 @@
+type Voter = string | number;
+
+function vote(voter: Voter): string {
+  if (typeof voter === "string") {
+    return `${voter}님이 찬성`;
+  } else if (typeof voter === "number") {
+    return `${voter}님이 반대`;
+  } else {
+    return "투표자 정보를 알 수 없음";
+  }
+}
+
+console.log(vote("반대"));
+console.log(vote(1024));
